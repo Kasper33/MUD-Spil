a0997db7ef81280adf1dc88e3bafa9eea143be65
CandyguyTheMemester <kasper_cool_007_@hotmail.com>
2016-10-05 10:21:00 +0200

    Tilføjede rum

--- ConsoleApplication6/world.cs
+++ ConsoleApplication6/world.cs
@@ -12,10 +12,21 @@ namespace ConsoleApplication6
         public World()
         {
             rooms = new List<Room>();
-            Room r1 = new Room("Mark", "En stor og grøn mark med masser af smukke planter og dyr");
-            Room r2 = new Room("", "");
-            Room r3 = new Room("", "");
-            Room r4 = new Room("", "");
+            Room r1 = new Room("Start", "En stor og grøn mark med masser af smukke planter og dyr");
+            Room r2 = new Room("Forladt Hus", "");
+            Room r3 = new Room("Helbredelses fontæne", "");
+            Room r4 = new Room("Mørk Skov", "");
+            Room r5 = new Room("Monster og kryds", "");
+            Room r6 = new Room("Monster", "");
+            Room r7 = new Room("Boss 1", "");
+            Room r8 = new Room("Mørk skov slut", "");
+            Room r9 = new Room("Ødelagt Karavane", "");
+            Room r10 = new Room("By", "");
+            Room r11 = new Room("Sort gyde", "");
+            Room r12 = new Room("By Kryds", "");
+            Room r13 = new Room("Butik", "");
+            Room r14 = new Room("Gammelt ødelagt hus", "");
+            Room r15 = new Room("Gammel Mand", "");
 
             rooms.Add(r1);
             rooms.Add(r2);
