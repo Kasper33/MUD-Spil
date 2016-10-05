684150a00b5ca2a7867a66ee26a37efc0673ad34
CandyguyTheMemester <kasper_cool_007_@hotmail.com>
2016-10-05 10:01:18 +0200

    Test

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Test");
             Console.ReadLine();
         }
     }
